e0c10c32d48d4a17cce8f9cf3310b618fa49cd8f
Kinrany <kinrany@yandex.com>
2019-06-15 16:52:43 +0300

    Make a brick move

--- client/index.js
+++ client/index.js
@@ -1,9 +1,40 @@
+const socket = io().connect();
+
 const canvas = /** @type {HTMLCanvasElement} */ (document.getElementById('canvas'));
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 const ctx = canvas.getContext('2d');
 
-const margin = 10;
-ctx.fillStyle = 'lightgreen';
-ctx.fillRect(margin, margin, canvas.width - margin * 2, canvas.height - margin * 2);
+// moving brick
+let point = {x: 50, y: 50};
+const brick_size = {
+  width: 30,
+  height: 30,
+};
+
+function draw() {
+  // background
+  ctx.fillStyle = 'lightgreen';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  // brick
+  ctx.fillStyle = 'red';
+  ctx.fillRect(
+    point.x - brick_size.width/2,
+    point.y - brick_size.height/2,
+    brick_size.width,
+    brick_size.height,
+  );
+}
+
+draw();
+
+socket.on('message', (event) => {
+  switch (event.tag) {
+    case 'brick_moved':
+      point = event.value;
+      break;
+  }
+  draw();
+});
--- server/index.js
+++ server/index.js
@@ -1,11 +1,42 @@
 const http = require('http');
 const express = require('express');
+const socketIo = require('socket.io');
 
 const app = express();
 
 app.use(express.static('client'));
 
 const server = http.createServer(app);
+const io = socketIo(server);
 server.listen(8000, () => {
   console.log('listening on *:8000');
 });
+
+/** @type {Map<symbol, SocketIO.Socket>} */
+const sockets = new Map();
+
+io.on('connection', (socket) => {
+  const symbol = Symbol();
+  sockets.set(symbol, socket);
+
+  console.log('A user connected');
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+    sockets.delete(symbol);
+  })
+});
+
+// moving brick
+let point = {x: 50, y: 50};
+
+setInterval(() => {
+  for (const socket of sockets.values()) {
+    point.x += 5;
+    point.y += 5;
+    socket.send({
+      tag: 'brick_moved',
+      value: point,
+    });
+  }
+}, 1000);
