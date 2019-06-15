191a477fcf60a637eb49b004750b73af892015e1
Kinrany <kinrany@yandex.com>
2019-06-15 16:13:21 +0300

    Light green background

--- client/index.js
+++ client/index.js
@@ -5,6 +5,5 @@ canvas.height = window.innerHeight;
 const ctx = canvas.getContext('2d');
 
 const margin = 10;
-
-ctx.fillStyle = 'red';
+ctx.fillStyle = 'lightgreen';
 ctx.fillRect(margin, margin, canvas.width - margin * 2, canvas.height - margin * 2);
