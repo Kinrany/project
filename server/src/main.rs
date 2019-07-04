39e9b4b8c852c409fc3412494480c5418a89c61b
Kinrany <kinrany@yandex.com>
2019-07-05 00:16:14 +0300

    Add a hello world server

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,6 +1,24 @@
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use game::Outcome;
+
+fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+fn index2() -> impl Responder {
+    HttpResponse::Ok().body("Hello world again!")
+}
 
 fn main() {
     println!("Hello, server!");
-    let _outcome = Outcome::start();
+    let addr = "127.0.0.1:8080";
+    HttpServer::new(|| {
+        App::new()
+        .route("/", web::get().to(index))
+        .route("/again", web::get().to(index2))
+    })
+    .bind(addr)
+    .expect(&("Failed to bind to ".to_string() + addr))
+    .run()
+    .expect("Failed to run");
 }
