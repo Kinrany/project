022f091149d1c7e3e973687a55e198db131f219a
Kinrany <kinrany@yandex.com>
2019-07-16 01:28:45 +0300

    Format source

--- game/src/lib.rs
+++ game/src/lib.rs
@@ -1,26 +1,25 @@
 pub enum Outcome {
-    Win,
-    Fail,
-    Undecided
+  Win,
+  Fail,
+  Undecided,
 }
 
 impl Outcome {
-    pub fn start() -> Outcome {
-        Outcome::Undecided
-    }
+  pub fn start() -> Outcome {
+    Outcome::Undecided
+  }
 
-    pub fn won(self) -> Result<Outcome, &'static str> {
-        match self {
-            Outcome::Undecided => Ok(Outcome::Win),
-            _ => Err("can only win while playing"),
-        }
+  pub fn won(self) -> Result<Outcome, &'static str> {
+    match self {
+      Outcome::Undecided => Ok(Outcome::Win),
+      _ => Err("can only win while playing"),
     }
+  }
 
-    pub fn failed(self) -> Result<Outcome, &'static str> {
-        match self {
-            Outcome::Undecided => Ok(Outcome::Fail),
-            _ => Err("can only lose while playing"),
-        }
+  pub fn failed(self) -> Result<Outcome, &'static str> {
+    match self {
+      Outcome::Undecided => Ok(Outcome::Fail),
+      _ => Err("can only lose while playing"),
     }
+  }
 }
-
