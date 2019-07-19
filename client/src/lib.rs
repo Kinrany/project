5826f415ccda6971064b300056af4702f6632cda
Kinrany <kinrany@yandex.com>
2019-07-20 02:38:46 +0300

    Add websocket example on client

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -1,24 +1,61 @@
 pub mod canvas;
 pub mod context2d;
+pub mod log;
 mod face;
 
 use {
   game::Outcome,
   wasm_bindgen::prelude::*,
+  wasm_bindgen::JsCast,
+  web_sys::{ErrorEvent, MessageEvent, WebSocket},
 };
 
-#[wasm_bindgen]
-extern "C" {
-  #[wasm_bindgen(js_namespace = console)]
-  fn log(s: &str);
+macro_rules! log {
+  ($($t:tt)*) => (log::log(&format_args!($($t)*).to_string()))
 }
 
 #[wasm_bindgen(start)]
 pub fn start() -> Result<(), JsValue> {
+  log!("start");
+
   let cvs = canvas::get();
   let ctx = context2d::new(cvs);
 
   face::draw(ctx);
+
+  let ws = WebSocket::new("ws://localhost:8080/ws")?;
+  log!("websocket created");
+
+  // create callback
+  let onmessage_callback = Closure::wrap(Box::new(move |e: MessageEvent| {
+    // handle message
+    let response = e
+      .data()
+      .as_string()
+      .expect("Can't convert received data to a string");
+    log!("message event, received data: {:?}", response);
+  }) as Box<dyn FnMut(MessageEvent)>);
+  // set message event handler on WebSocket
+  ws.set_onmessage(Some(onmessage_callback.as_ref().unchecked_ref()));
+  // forget the callback to keep it alive
+  onmessage_callback.forget();
+
+  let onerror_callback = Closure::wrap(Box::new(move |e: ErrorEvent| {
+    log!("error event: {:?}", e);
+  }) as Box<dyn FnMut(ErrorEvent)>);
+  ws.set_onerror(Some(onerror_callback.as_ref().unchecked_ref()));
+  onerror_callback.forget();
+
+  let cloned_ws = ws.clone();
+  let onopen_callback = Closure::wrap(Box::new(move |_| {
+    log!("socket opened");
+    match cloned_ws.send_with_str("ping") {
+      Ok(_) => log!("message successfully sent"),
+      Err(err) => log!("error sending message: {:?}", err),
+    }
+  }) as Box<dyn FnMut(JsValue)>);
+  ws.set_onopen(Some(onopen_callback.as_ref().unchecked_ref()));
+  onopen_callback.forget();
 
   Ok(())
 }
