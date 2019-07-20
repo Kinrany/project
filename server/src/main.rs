8a7fcc6e989b0193fc3664a20df874302a7fde6d
Kinrany <kinrany@yandex.com>
2019-07-21 01:50:02 +0300

    Add echo response

--- server/src/main.rs
+++ server/src/main.rs
@@ -4,14 +4,13 @@ use {
   actix_web::{
     web,
     App,
-    Error,
     HttpRequest,
-    HttpResponse,
     HttpServer,
   },
   actix_web_actors::ws,
 };
 
+#[derive(Clone, Copy)]
 struct MyWebSocket(());
 
 impl MyWebSocket {
@@ -27,28 +26,31 @@ impl Actor for MyWebSocket {
 }
 
 impl StreamHandler<ws::Message, ws::ProtocolError> for MyWebSocket {
-  fn handle(&mut self, msg: ws::Message, _ctx: &mut Self::Context) {
+  fn handle(&mut self, msg: ws::Message, ctx: &mut Self::Context) {
     println!("WS: {:?}", msg);
+    match msg {
+      ws::Message::Text(text) => ctx.text(text),
+      _ => {},
+    };
   }
-}
-
-fn websocket_index(r: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
-  ws::start(MyWebSocket::new(), &r, stream)
 }
 
 fn main() {
   println!("Hello, server!");
+
+  let _game = game::Game::new(12345678);
+
+  let websocket = MyWebSocket::new();
+  let websocket_handler = move |r: HttpRequest, stream: web::Payload| ws::start(websocket, &r, stream);
+
   let addr = "127.0.0.1:8080";
-  HttpServer::new(|| {
-    App::new()
-      .service(fs::Files::new("/static", "server/static").show_files_listing())
-      .service(fs::Files::new("/wasm", "client/pkg").show_files_listing())
-      .service(web::resource("/ws").route(web::get().to(websocket_index)))
-  })
+  HttpServer::new(move || App::new()
+    .service(fs::Files::new("/static", "server/static").show_files_listing())
+    .service(fs::Files::new("/wasm", "client/pkg").show_files_listing())
+    .service(web::resource("/ws").route(web::get().to(websocket_handler)))
+  )
   .bind(addr)
   .expect(&("Failed to bind to ".to_string() + addr))
   .run()
   .expect("Failed to run");
-
-  let _game = game::Game::new(12345678);
 }
